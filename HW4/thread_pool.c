3b40c9cfd1d19c287cb96dcf87f7d225522d5e4f
Ignat <petrov-ignat85@mail.ru>
2023-04-16 22:38:46 +0300

    update 4th hw

--- HW4/thread_pool.c
+++ HW4/thread_pool.c
@@ -2,6 +2,7 @@
 #include <pthread.h>
 #include "stdlib.h"
 #include <stdatomic.h>
+#include <math.h>
 
 enum {
     TPOOL_STATUS_IN_POOL = 1,
@@ -16,6 +17,8 @@ struct thread_task {
     /* PUT HERE OTHER MEMBERS */
     atomic_int status;
     atomic_bool detach;
+    pthread_mutex_t mutex;
+    pthread_cond_t is_finish;
     struct thread_task *next;
     void *result;
 };
@@ -33,16 +36,11 @@ struct thread_pool {
     int max_thread_count;
     struct queue *task_queue;
     int thread_count;
+    int *threads_status;
+    atomic_int task_count;
     pthread_mutex_t queue_lock;
     pthread_mutex_t status_lock;
-    int *threads_status;
 };
-
-double get_time() {
-    struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    return ts.tv_sec + ts.tv_nsec/1000000000;
-}
 
 void queue_push(struct queue *queue, struct thread_task *task) {
     if (queue->size == 0) {
@@ -68,7 +66,7 @@ struct thread_args {
 };
 
 void *thread_func(void *arguments) {
-    struct thread_args *args = (struct thread_args*) arguments;
+    struct thread_args *args = (struct thread_args *) arguments;
 
     pthread_mutex_lock(&args->pool->status_lock);
     args->pool->threads_status[args->thread_id] = 2;
@@ -83,10 +81,16 @@ void *thread_func(void *arguments) {
         struct thread_task *task = queue_pop(args->pool->task_queue);
         pthread_mutex_unlock(&args->pool->queue_lock);
 
-        task->status = TPOOL_STATUS_RUNNING;
+        atomic_store(&task->status, TPOOL_STATUS_RUNNING);
         task->result = task->function(task->arg);
-        task->status = TPOOL_STATUS_FINISHED;
-        if (task->detach) free(task);
+        atomic_store(&task->status, TPOOL_STATUS_FINISHED);
+        pthread_cond_broadcast(&task->is_finish);
+        atomic_fetch_sub(&args->pool->task_count, 1);
+
+        if (atomic_load(&task->detach)) {
+            atomic_store(&task->status, 0);
+            thread_task_delete(task);
+        }
     }
 
     pthread_mutex_lock(&args->pool->status_lock);
@@ -109,6 +113,7 @@ thread_pool_new(int max_thread_count, struct thread_pool **pool) {
     (*pool)->threads_status = calloc(max_thread_count, sizeof(int));
     pthread_mutex_init(&(*pool)->queue_lock, NULL);
     pthread_mutex_init(&(*pool)->status_lock, NULL);
+    atomic_init(&(*pool)->task_count, 0);
     for (int i = 0; i < max_thread_count; i++) (*pool)->threads_status[i] = 0;
     return 0;
 }
@@ -120,13 +125,10 @@ thread_pool_thread_count(const struct thread_pool *pool) {
 
 int
 thread_pool_delete(struct thread_pool *pool) {
-    if (pool->task_queue->size > 0) return TPOOL_ERR_HAS_TASKS;
-    for (int i = 0; i < pool->max_thread_count; i++) {
-        if (pool->threads_status[i] == 2) return TPOOL_ERR_HAS_TASKS;
-    }
+    if (atomic_load(&pool->task_count) > 0) return TPOOL_ERR_HAS_TASKS;
+    free(pool->threads);
     free(pool->task_queue);
     free(pool->threads_status);
-    free(pool->threads);
     pthread_mutex_destroy(&pool->queue_lock);
     pthread_mutex_destroy(&pool->status_lock);
     free(pool);
@@ -135,21 +137,25 @@ thread_pool_delete(struct thread_pool *pool) {
 
 int
 thread_pool_push_task(struct thread_pool *pool, struct thread_task *task) {
-    if (pool->task_queue->size == TPOOL_MAX_TASKS) return TPOOL_ERR_TOO_MANY_TASKS;
-    task->status = TPOOL_STATUS_IN_POOL;
+    if (atomic_load(&pool->task_count) == TPOOL_MAX_TASKS) return TPOOL_ERR_TOO_MANY_TASKS;
+    atomic_fetch_add(&pool->task_count, 1);
+    atomic_store(&task->status, TPOOL_STATUS_IN_POOL);
     pthread_mutex_lock(&pool->queue_lock);
     queue_push(pool->task_queue, task);
     pthread_mutex_unlock(&pool->queue_lock);
+
+    pthread_mutex_lock(&pool->status_lock);
     for (int i = 0; i < pool->max_thread_count; i++) {
         if (pool->threads_status[i] < 2) {
             if (pool->threads_status[i] == 0) pool->thread_count++;
             struct thread_args *args = malloc(sizeof(struct thread_args));
             args->pool = pool;
             args->thread_id = i;
-            pthread_create(&pool->threads[i], NULL, thread_func, (void *)args);
+            pthread_create(&pool->threads[i], NULL, thread_func, (void *) args);
             break;
         }
     }
+    pthread_mutex_unlock(&pool->status_lock);
     return 0;
 }
 
@@ -158,50 +164,85 @@ thread_task_new(struct thread_task **task, thread_task_f function, void *arg) {
     (*task) = malloc(sizeof(struct thread_task));
     (*task)->function = function;
     (*task)->arg = arg;
-    (*task)->status = 0;
-    (*task)->detach = false;
+    atomic_init(&(*task)->status, 0);
+    atomic_init(&(*task)->detach, false);
+    pthread_mutex_init(&(*task)->mutex, NULL);
+    pthread_cond_init(&(*task)->is_finish, NULL);
     return 0;
 }
 
 bool
 thread_task_is_finished(const struct thread_task *task) {
-    return task->status == TPOOL_STATUS_FINISHED;
+    return atomic_load(&task->status) == TPOOL_STATUS_FINISHED;
 }
 
 bool
 thread_task_is_running(const struct thread_task *task) {
-    return task->status == TPOOL_STATUS_RUNNING;
+    return atomic_load(&task->status) == TPOOL_STATUS_RUNNING;
 }
 
 int
-thread_task_join(struct thread_task *task, double timeout, void **result) {
-    if (task->status == 0) return TPOOL_ERR_TASK_NOT_PUSHED;
-    double start = get_time();
-    for (;;) {
-        if (get_time() - start > timeout) return TPOOL_ERR_TIMEOUT;
-        if (task->status == TPOOL_STATUS_FINISHED) {
-            task->status = 0;
-            *result = task->result;
-            return 0;
-        }
-    }
+thread_task_join(struct thread_task *task, void **result) {
+    if (atomic_load(&task->status) == 0) return TPOOL_ERR_TASK_NOT_PUSHED;
+
+    pthread_mutex_lock(&task->mutex);
+    if (atomic_load(&task->status) != TPOOL_STATUS_FINISHED)
+        pthread_cond_wait(&task->is_finish, &task->mutex);
+    atomic_store(&task->status, 0);
+    *result = task->result;
+    pthread_mutex_unlock(&task->mutex);
+    return 0;
 }
 
 int
 thread_task_delete(struct thread_task *task) {
-    if (task->status != 0) return TPOOL_ERR_TASK_IN_POOL;
+    if (atomic_load(&task->status) != 0) return TPOOL_ERR_TASK_IN_POOL;
+    pthread_cond_destroy(&task->is_finish);
+    pthread_mutex_destroy(&task->mutex);
     free(task);
     return 0;
 }
 
+#ifdef NEED_TIMED_JOIN
+
+int
+thread_task_timed_join(struct thread_task *task, double timeout, void **result) {
+    if (atomic_load(&task->status) == 0) return TPOOL_ERR_TASK_NOT_PUSHED;
+    if (timeout <= 0) return TPOOL_ERR_TIMEOUT;
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    time_t int_part;
+    long nano_part = modf(timeout, &int_part) * 1000000000;
+    ts.tv_sec += int_part;
+    ts.tv_nsec += nano_part;
+
+    pthread_mutex_lock(&task->mutex);
+    int rc;
+    if (atomic_load(&task->status) != TPOOL_STATUS_FINISHED)
+        rc = pthread_cond_timedwait(&task->is_finish, &task->mutex, &ts);
+    if (rc == ETIMEDOUT) {
+        pthread_mutex_unlock(&task->mutex);
+        return TPOOL_ERR_TIMEOUT;
+    }
+    atomic_store(&task->status, 0);
+    *result = task->result;
+    pthread_mutex_unlock(&task->mutex);
+
+    return 0;
+}
+
+#endif
+
 #ifdef NEED_DETACH
 
 int
-thread_task_detach(struct thread_task *task)
-{
-    if (task->status == 0) return TPOOL_ERR_TASK_NOT_PUSHED;
-    if (task->status == TPOOL_STATUS_FINISHED) free(task);
-    else task->detach = true;
+thread_task_detach(struct thread_task *task) {
+    if (atomic_load(&task->status) == 0) return TPOOL_ERR_TASK_NOT_PUSHED;
+    if (atomic_load(&task->status) == TPOOL_STATUS_FINISHED) {
+        atomic_store(&task->status, 0);
+        thread_task_delete(task);
+    }
+    else atomic_store(&task->detach, true);
     return 0;
 }
 
--- HW4/thread_pool.h
+++ HW4/thread_pool.h
@@ -1,8 +1,23 @@
+#pragma once
+
 #include <stdbool.h>
-#ifndef THREAD_POOL_DEFINED
-#define THREAD_POOL_DEFINED
+
+/**
+ * Here you should specify which features do you want to implement via macros:
+ * NEED_DETACH and NEED_TIMED_JOIN. If you want to enable detach, do:
+ *
+ *     #define NEED_DETACH
+ *
+ * To enable timed join do:
+ *
+ *     #define NEED_TIMED_JOIN
+ *
+ * It is important to define these macros here, in the header, because it is
+ * used by tests.
+ */
 
 #define NEED_DETACH
+#define NEED_TIMED_JOIN
 
 struct thread_pool;
 struct thread_task;
@@ -20,8 +35,8 @@ enum thread_poool_errcode {
 	TPOOL_ERR_HAS_TASKS,
 	TPOOL_ERR_TASK_NOT_PUSHED,
 	TPOOL_ERR_TASK_IN_POOL,
-    TPOOL_ERR_TIMEOUT,
 	TPOOL_ERR_NOT_IMPLEMENTED,
+	TPOOL_ERR_TIMEOUT,
 };
 
 /** Thread pool API. */
@@ -111,7 +126,26 @@ thread_task_is_running(const struct thread_task *task);
  *     - TPOOL_ERR_TASK_NOT_PUSHED - task is not pushed to a pool.
  */
 int
-thread_task_join(struct thread_task *task, double timeout, void **result);
+thread_task_join(struct thread_task *task, void **result);
+
+#ifdef NEED_TIMED_JOIN
+
+/**
+ * Like thread_task_join() but wait no longer than the timeout.
+ * @param task Task to join.
+ * @param timeout Timeout in seconds. 0 means no waiting at all. For an infinite
+ *   timeout pass infinity or DBL_MAX or just something huge.
+ * @param[out] result Pointer to stored result of @a task.
+ *
+ * @retval 0 Success.
+ * @retval != 0 Error code.
+ *     - TPOOL_ERR_TASK_NOT_PUSHED - task is not pushed to a pool.
+ *     - TPOOL_ERR_TIMEOUT - join timed out, nothing is done.
+ */
+int
+thread_task_timed_join(struct thread_task *task, double timeout, void **result);
+
+#endif
 
 /**
  * Delete a task, free its memory.
@@ -141,5 +175,3 @@ int
 thread_task_detach(struct thread_task *task);
 
 #endif
-
-#endif /* THREAD_POOL_DEFINED */
